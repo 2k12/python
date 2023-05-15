f5bfd45bae20910c77f6ec3682c0bdc4e901be86
Joan <jeipige@gmail.com>
2023-05-15 00:50:32 -0500

    "metodos hasta primos"

--- ejerciciosbasicos.py
+++ ejerciciosbasicos.py
@@ -1,3 +1,4 @@
+from math import *
 #Escribe un programa que calcule la suma de dos números ingresados por el usuario.
 
 def ejerciciosbasicos1():
@@ -52,3 +53,66 @@ def factorial():
 
 
 #factorial()
+
+
+def suma_promedio_desviacionestandar():
+    salir=" "
+    valor=0
+    lista = []
+    aux=0
+    res=0
+    iterador=0
+
+    #variables para la desviacion
+    u = 0
+    distancia = []
+    sumatoria=0
+
+    print("ingrese una lista de numeros y luego ingrese la palabra calcular!: ")
+    while salir ==" ":
+        valor = input("numeros: ")
+        if valor== "calcular":
+            salir == valor
+            break
+        else:
+            lista.append(int(valor))
+            iterador += 1
+
+    for n in lista:
+        aux = int(n)
+        res += aux     
+
+    u = res/iterador  
+
+    for  n in lista:
+        distancia.append(pow(n-u,2))
+
+    for nl in distancia:
+        aux2 = nl 
+        sumatoria += aux2
+    
+
+    print (f"LA SUMA ES: {res} || EL PROMEDIO ES: {u} || LA DESVIACION ES: { sqrt(sumatoria/iterador) }")
+
+
+#suma_promedio_desviacionestandar()
+
+def primos():
+    
+    n=int(input("Ingrese un numero: "))
+    contador=0
+    res = ""
+    while n == 1:
+        n=int(input("Ingrese un numero distinto de -> 1: "))
+
+    for x in range(2,100000):
+        if  n % x == 0:
+            contador+=1
+            if contador <= 2:
+                res = "primo"
+            else:
+                res= "no primo"
+    print(f"numero {n} es {res} ")
+
+
+#primos()
